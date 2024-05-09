42c9eb6d1e81e26dbe5fd3f56cb6cabe88ad603d
Dani Rogmans <danirogmans@gmail.com>
2024-05-09 11:37:05 +0200

    finishing the model fetching aspect

--- utils.py
+++ utils.py
@@ -12,8 +12,8 @@ def fetch_model_from_s3_bucket(model_name):
         return
     s3 = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
                       aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
-    s3.download_file('url-phising-models', model_name, destination_path)
+    s3.download_file('url-phishing-models', model_name, destination_path)
 
 
 if __name__ == "__main__":
-    fetch_model_from_s3_bucket("test_model.h5")
+    fetch_model_from_s3_bucket("model.h5")
